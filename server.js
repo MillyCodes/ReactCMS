7f1d707d423f107c0ed8f6bb715a33236d014ee6
Milly Rodriguez <millz118@gmail.com>
2018-10-26 12:26:38 -0400

    mongoose db

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 4444;
 
+//mongoose
+const mongoose = require("mongoose");
+
+require("dotenv").config({ path: "variables.env" });
+
+mongoose
+    .connect(process.env.MONO_URI)
+    .then(() => console.log("db connected"))
+    .catch(err => console.error(err));
+
 app.listen(PORT, () => {
     console.log("Listening on PORT 4444");
 });
