05d7280417f8c3c19fd1c3e5ee36bbad27499875
Milly Rodriguez <millz118@gmail.com>
2018-10-26 14:43:17 -0400

    added GraphQL schema-types

--- server.js
+++ server.js
@@ -1,20 +1,64 @@
 const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
 const app = express();
 const PORT = 4444;
 
+// Schema
+const Blog = require("./models/Blog");
+const User = require("./models/User");
+//Graphql Schema types
+const typeDefs = gql`
+    type Blog {
+        title: String!
+        author: String!
+        body: String
+        date: String
+    }
+    type User {
+        username: String!
+        password: String!
+        email: String!
+        date: String
+    }
+    type Query {
+        showAllBlogs: [Blog]
+    }
+`;
+
+const resolvers = {
+    Query: () => {}
+};
+
 //mongoose
 const mongoose = require("mongoose");
-
+//dotenv variables
 require("dotenv").config({ path: "variables.env" });
 
 mongoose
-    .connect(process.env.MONO_URI)
+    .connect(
+        process.env.MONGO_URI,
+        {
+            useNewUrlParser: true,
+            useCreateIndex: true
+        }
+    )
     .then(() => console.log("db connected"))
     .catch(err => console.error(err));
 
-app.listen(PORT, () => {
-    console.log("Listening on PORT 4444");
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: {
+        Blog,
+        User
+    }
 });
+
+server.applyMiddleware({ app });
+
+app.listen({ port: PORT }, () =>
+    console.log(`🚀 Server ready at http://localhost:4444${server.graphqlPath}`)
+);
 
 app.get("/", function(req, res) {
     res.send("Hello World");
